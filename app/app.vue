256462c6b9355317bf996537034d12faa50d0982
Caleb Lee <caleb.gwl@gmail.com>
2017-02-28 16:29:20 -0600

    Rough mock-up of app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,21 @@
 <template lang="html">
-  <div class="">
+  <div class="app">
+    <div class="page">
+      <div class="panel">
+        <div class="panel-block">
 
+          <div class="results">
+            <div class="track">
+              <div class="track__frame">
+                <img src="http://placecera.com/100/100" alt="" class="track__media">
+              </div>
+              <h1 class="track__artist">Katy Perry</h1>
+              <p class="track__name">Firework</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
